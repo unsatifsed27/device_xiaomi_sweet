6e0c4351b9e83004724e2f0a4617c17917baa4b9
baalajimaestro <me@baalajimaestro.me>
2022-02-04 23:27:43 +0300

    sweet: Optimise dex flags

--- device.mk
+++ device.mk
@@ -107,6 +107,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
+
 # Recovery
 PRODUCT_PACKAGES += \
     fastbootd
